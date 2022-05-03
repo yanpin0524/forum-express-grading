bed8b515c410dc5ce81a3725d0538ee4c37fe246
yanpin0524 <a131234568@gmail.com>
2022-05-03 21:15:49 +0800

    feat: add api-auth middleware

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -5,12 +5,13 @@ const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
 const userController = require('../../controllers/apis/user-controller')
 const { apiErrorHandler } = require('../../middleware/error-handler')
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 
-router.use('/admin', admin)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
+
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
-
-router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', apiErrorHandler)
 
